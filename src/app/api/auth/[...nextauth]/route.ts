c07a08e347dfeb67948a863de27d646a109bc332
Enkratia <manofthetreasure@gmail.com>
2023-11-30 23:14:28 +0300

    m

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,67 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import NextAuth from "next-auth/next";
+
+import { Backend_URL } from "@/lib/Constants";
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "email@email.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials?.password) return null;
+        const { email, password } = credentials;
+
+        const res = await fetch(Backend_URL + "/auth/login", {
+          method: "POST",
+          body: JSON.stringify({
+            email,
+            password,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (res.status === 401) {
+          console.log(res.statusText);
+          return null;
+        }
+
+        const user = await res.json();
+        return user;
+      },
+    }),
+  ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        return { ...token, ...user };
+      }
+
+      return token;
+    },
+
+    async session({ token, session }) {
+      session.user = token.user;
+      session.backendTokens = token.backendTokens;
+
+      return session;
+    },
+  },
+};
+
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
